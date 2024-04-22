2c55412124f25f8c92177d28de95fff947c78649
fluriafloral <lampia131@gmail.com>
2024-04-22 14:19:32 -0300

    continuação appservice

--- src/AppService.java
+++ src/AppService.java
@@ -2,11 +2,19 @@ package src;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class AppService {
+
+    private final static String NAME = "temperature_converter";
+    private final static String HOST = "localhost"; 
+    private final static int PORT = 8081;
     
     private static ServerSocket serverSocket;
     private static Socket socket;
@@ -19,6 +27,18 @@ public class AppService {
         try {
 
             socket = new Socket("localhost", 8080);
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            out.write(NAME);
+            out.newLine();
+
+            out.write(HOST);
+            out.newLine();
+
+            out.write(PORT);
+            out.newLine();
+
+            socket.close();
 
         } catch (Exception e) {
             
@@ -26,5 +46,51 @@ public class AppService {
 
         }
 
+    }
+
+    public static void serviceMenu() {
+
+        try {
+
+        } catch(Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        register();
+
+        try {
+
+            serverSocket = new ServerSocket(PORT);
+
+            System.out.println("Serviço de conversão iniciado com sucesso, rodando na porta 8081");
+            System.out.println("digite exit e aperte enter para encerrar o programa");
+
+            Scanner scanner = new Scanner(System.in);
+            String aux = scanner.nextLine();
+            while(!aux.equals("exit")) {
+
+                socket = serverSocket.accept();
+
+                socket.close();
+                aux = scanner.nextLine();
+
+            }
+            scanner.close();
+
+            serverSocket.close();
+
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+
+        }
+
+        
     }
 }
